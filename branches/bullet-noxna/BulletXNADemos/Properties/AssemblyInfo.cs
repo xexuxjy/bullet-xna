015f169275dd71aa41a5a4a26fb83a924fcd508b
andres.traks <andres.traks@2a7731ba-da3a-768a-5ded-3a4adfc91c65>
2011-05-14 11:34:50 +0000

    Start a branch to make BulletXNA independent of XNA. TODO: - Create Vector and Matrix math classes. These can be imported from SlimMath. - Create a solution without XNA dependencies. - Compare performance of using the custom math classes against using XNA classes directly.

---
+++ branches/bullet-noxna/BulletXNADemos/Properties/AssemblyInfo.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * C# / XNA  port of Bullet (c) 2011 Mark Neale <xexuxjy@hotmail.com>
+ *
+ * Bullet Continuous Collision Detection and Physics Library
+ * Copyright (c) 2003-2008 Erwin Coumans  http://www.bulletphysics.com/
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose, 
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BulletXNADemos")]
+[assembly: AssemblyProduct("BulletXNADemos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f6e265b4-dd2f-4173-8302-f8bb60e25fb1")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]

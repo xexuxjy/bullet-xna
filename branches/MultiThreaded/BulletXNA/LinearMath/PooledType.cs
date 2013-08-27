26246b79ef4a9b5f0fb6f46631ee2b31e7dbff3f
markneale@hotmail.com <markneale@hotmail.com@2a7731ba-da3a-768a-5ded-3a4adfc91c65>
2013-08-27 18:31:54 +0000

    locked pool

--- branches/MultiThreaded/BulletXNA/LinearMath/PooledType.cs
+++ branches/MultiThreaded/BulletXNA/LinearMath/PooledType.cs
@@ -28,4 +28,34 @@ namespace BulletXNA.LinearMath
 
         private Stack<T> m_pool = new Stack<T>();
     }
+
+    public class LockedPooledType<T> where T : new()
+    {
+
+        public virtual T Get()
+        {
+            lock (this)
+            {
+                if (m_pool.Count == 0)
+                {
+                    m_pool.Push(new T());
+                }
+                return m_pool.Pop();
+            }
+        }
+
+        public virtual void Free(T obj)
+        {
+            Debug.Assert(!m_pool.Contains(obj));
+            lock (this)
+            {
+                m_pool.Push(obj);
+            }
+        }
+
+
+        private Stack<T> m_pool = new Stack<T>();
+    }
+
+
 }
